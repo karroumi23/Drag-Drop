f074e52b72db0f2e9d63d5bd3b73be913bae78d1
karroumi <anasskarroumi2016@gmail.com>
2024-04-01 23:32:12 +0200

    Create add function from input to boxes

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 let btn = document.getElementById("btn");
 let inp = document.getElementById("inp");
 let boxs = document.querySelectorAll(".box");
+//CREATE FUNCTION CONVERT INPUT VALUE TO BOX
+
+btn.onclick = function () {
+  if (inp.value != "") {
+    boxs[0].innerHTML += `
+     <p class="item" draggable="true" >${inp.value}</p>`;
+    inp.value = "";
+  }
+};
