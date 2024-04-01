09c7a8f2eaef981129124c928fadce0780e4aa3b
karroumi <anasskarroumi2016@gmail.com>
2024-04-02 01:12:50 +0200

    create functions drag and drop

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById("btn");
 let inp = document.getElementById("inp");
 let boxs = document.querySelectorAll(".box");
+let drag = null;
+
 //CREATE FUNCTION CONVERT INPUT VALUE TO BOX
 
 btn.onclick = function () {
@@ -9,4 +11,40 @@ btn.onclick = function () {
      <p class="item" draggable="true" >${inp.value}</p>`;
     inp.value = "";
   }
+  dragitem();
 };
+
+// create function drag and drop
+
+function dragitem() {
+  let items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    item.addEventListener("dragstart", function () {
+      drag = item;
+      item.style.opacity = "0.5";
+    });
+    item.addEventListener("dragend", function () {
+      drag = null;
+      item.style.opacity = "1";
+    });
+    boxs.forEach((box) => {
+      box.addEventListener("dragover", function (e) {
+        //Cancel (annuler) default of this event 
+        e.preventDefault();
+        //change style
+        this.style.background = "green";
+        this.style.color = "white";
+      });
+      box.addEventListener("dragleave", function () {
+        this.style.background = "white";
+        this.style.color = "black";
+      });
+
+      box.addEventListener("drop", function () {
+        this.append(drag);
+        this.style.background = "white";
+        this.style.color = "black";
+      });
+    });
+  });
+}
